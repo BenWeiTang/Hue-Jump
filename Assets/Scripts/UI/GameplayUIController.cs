819b6f6026d74881e5c06e1d23a64f565a50f60f
Ben Tang <sjps30503@gmail.com>
2023-02-13 22:36:20 -0500

    Added Score

--- Assets/Scripts/UI/GameplayUIController.cs
+++ Assets/Scripts/UI/GameplayUIController.cs
@@ -1,23 +1,36 @@
-using System;
+using TMPro;
 using UnityEngine;
 
 public class GameplayUIController : MonoBehaviour
 {
     [SerializeField] private CanvasGroup _inGamePanel;
     [SerializeField] private CanvasGroup _endGamePanel;
+    [SerializeField] private Transform _camera;
+    [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private TextMeshProUGUI _finalScoreText;
+    
+    private float _cameraInitialHeight;
 
     public void RestartGame() => GameManager.Instance.RestartGame();
-    private void Awake() => GameManager.Instance.GameEnded += OnGameEnded;
-    private void OnDestroy() => GameManager.Instance.GameEnded -= OnGameEnded;
 
     private void Start()
     {
+        GameManager.Instance.GameEnded += OnGameEnded;
         _inGamePanel.alpha = 1;
         _inGamePanel.interactable = false;
         _inGamePanel.blocksRaycasts = false;
         _endGamePanel.alpha = 0;
         _endGamePanel.interactable = false;
         _endGamePanel.blocksRaycasts = false;
+
+        _cameraInitialHeight = _camera.position.y;
+    }
+    
+    private void OnDestroy() => GameManager.Instance.GameEnded -= OnGameEnded;
+
+    private void Update()
+    {
+        _scoreText.text = (_camera.position.y - _cameraInitialHeight).ToString("0.00");
     }
 
     private void OnGameEnded()
@@ -26,5 +39,6 @@ public class GameplayUIController : MonoBehaviour
         _endGamePanel.alpha = 1;
         _endGamePanel.interactable = true;
         _endGamePanel.blocksRaycasts = true;
+        _finalScoreText.text = _scoreText.text;
     }
 }
